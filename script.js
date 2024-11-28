254fee7d6909171caaf26343d7b9a1696c27e92c
Sonia Tyburczy <styburczy.business@gmail.com>
2024-11-27 19:28:46 -0500

    PROG BAR ARTIFACTS

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let currentProgress = 25;
-const button = document.getElementById('progress-button');
+let currentProgress = 0;
+const bar = document.getElementById('progress-bar');
+const iframe = document.querySelector('iframe');
 
 function progressUp() {
     currentProgress += 25;
-    button.setAttribute('aria-label', currentProgress)
+    bar.setAttribute('aria-label', currentProgress);
+    bar.style.width = currentProgress + '%'; 
+    console.log(currentProgress);
+}
+
+function moveOn() {
+    document.getElementById('video-column').style.display = 'none';
+    document.getElementById('summary-column').style.display = 'none';
 }
